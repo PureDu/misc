f04e06ff4f89eb0d150d5803e112eb4c07550742
duxiaobo <848295325@qq.com>
2017-11-11 11:27:45 +0800

    项目类型改为 win32

--- cpp/learnsfml/CircleShape/main.cc
+++ cpp/learnsfml/CircleShape/main.cc
@@ -1,8 +1,21 @@
 #include <SFML/Graphics.hpp>
+#include <windows.h>
+
+// Inform the Nvidia/AMD driver that this SFML application could
+// benefit from using the more powerful discrete GPU
+extern "C"
+{
+    __declspec(dllexport) DWORD NvOptimusEnablement = 0x00000001;
+    __declspec(dllexport) int AmdPowerXpressRequestHighPerformance = 1;
+}
+int main(int argc, char* argv[]);
+////////////////////////////////////////////////////////////
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, INT) {
+    return main(__argc, __argv);
+}
 
 
-
-int main() {
+int main(int argc, char* argv[]) {
     sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
